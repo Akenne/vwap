087cd54ba08da2a7bcfba4bc420c4ae5bca7d157
Adam Kenneweg <akennewe@uwaterloo.ca>
2021-03-24 14:58:31 -0400

    pull data

---
+++ vwap.py
@@ -0,0 +1,20 @@
+import cbpro
+
+class myWebsocketClient(cbpro.WebsocketClient):
+    def on_open(self):
+        self.url = "wss://ws-feed.pro.coinbase.com/"
+        self.products = ["BTC-USD"]
+        self.channels = ["matches"]
+        print("Lets count the messages!")
+    def on_message(self, msg):
+    	print(msg)
+    def on_close(self):
+        print("-- Goodbye! --")
+
+wsClient = myWebsocketClient()
+wsClient.start()
+print(wsClient.url, wsClient.products)
+while (wsClient.message_count < 500):
+    print ("\nmessage_count =", "{} \n".format(wsClient.message_count))
+    time.sleep(1)
+wsClient.close()
